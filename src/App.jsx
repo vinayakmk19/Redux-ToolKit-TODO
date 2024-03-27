2fb1593f23b6a15baf8a89a0dc025d97539321ac
vinayakmk19 <vinayakmkgen@gmail.com>
2024-03-27 18:19:04 +0530

    Add and Delete functionality is done update and toggle is pending

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+function App() {
+  return (
+    <>
+    <Provider store={store}>
+
+      <AddTodo />
+      <Todos />
+    </Provider>
+    </>
+  );
+}
+
+export default App;
